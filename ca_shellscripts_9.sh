2058fe194c6b44e6035e1c0cd3efda24e1b2becf
SkipperICE <121302022+SkipperICE@users.noreply.github.com>
2023-02-16 18:55:44 +0100

    fini

--- ca_shellscripts_9.sh
+++ ca_shellscripts_9.sh
@@ -1,1 +1,12 @@
 #ca_shellscripts_9.sh
+
+a=(3 5 8 10 6)
+b=(6 5 4 12)
+c=(14 7 5 7)
+
+for num in "${a[@]}"
+do
+  if [[ " ${b[@]} " =~ " $num " ]] && [[ " ${c[@]} " =~ " $num " ]]; then
+    echo $num
+  fi
+done
